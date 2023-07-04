f5f7c19de49e0647c632d948d4b4a77428d31c18
Vamshi krishna <vamshi@goniyo.com>
2023-07-04 18:23:54 +0530

    initial commit

---
+++ db/sqlc/store.go
@@ -0,0 +1,126 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+type Store struct {
+	*Queries
+	db *sql.DB //  required to add new DB txn
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err %v", err, rbErr)
+		}
+		return err
+	}
+	return tx.Commit()
+}
+
+// type TransferTxParams struct {
+// 	FromAccountID int64 `json:"from_account_id"`
+// 	ToAccountID   int64 `json:"to_account_id"`
+// 	Amount        int64 `json:"amount"`
+// }
+
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+func (store *Store) TransferTx(ctx context.Context, arg CreateTransferParams) (TransferTxResult, error) {
+	var result TransferTxResult
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(
+			ctx,
+			CreateEntryParams{
+				AccountID: arg.FromAccountID,
+				Amount:    -arg.Amount,
+			},
+		)
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = q.CreateEntry(
+			ctx,
+			CreateEntryParams{
+				AccountID: arg.ToAccountID,
+				Amount:    arg.Amount,
+			},
+		)
+		if err != nil {
+			return err
+		}
+
+		if arg.FromAccountID < arg.ToAccountID {
+			result.FromAccount, result.ToAccount, err = AddMoney(ctx, q, arg.FromAccountID, -arg.Amount, arg.ToAccountID, arg.Amount)
+			if err != nil {
+				return err
+			}
+		} else {
+			result.ToAccount, result.FromAccount, err = AddMoney(ctx, q, arg.ToAccountID, arg.Amount, arg.FromAccountID, -arg.Amount)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	return result, err
+}
+
+func AddMoney(
+	ctx context.Context,
+	q *Queries,
+	aID1 int64,
+	a1Amount int64,
+	aID2 int64,
+	a2Amount int64,
+) (a1 Account, a2 Account, err error) {
+	a1, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     aID1,
+		Amount: a1Amount,
+	})
+	if err != nil {
+		return
+	}
+	a2, err = q.AddAccountBalance(ctx, AddAccountBalanceParams{
+		ID:     aID2,
+		Amount: a2Amount,
+	})
+	if err != nil {
+		return
+	}
+	return
+}
